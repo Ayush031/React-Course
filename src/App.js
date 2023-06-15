1df50e8f7093128b8cb53da641c0461c7573975d
Ayush <ayushkansal303@gmail.com>
2023-06-15 19:00:55 +0530

    created dark mode for complete document

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
-import About from "./Components/About"
+// import About from "./Components/About"
+import About1 from "./Components/About1"
 
 import { useState } from "react";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [modeBtnTxt, setModeBtnTxt] = useState("Enable Dark Mode");
+
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
@@ -27,8 +29,9 @@ function App() {
     <>
       <Navbar title="AyVerse" mode={mode} toggleMode={toggleMode} modeBtnTxt={modeBtnTxt} />
       <div className="container my-3">
-        <TextForm heading="Enter Text Below: " mode={mode} />
-        <About />
+        <TextForm heading="Enter Text Below: " mode={mode} toggleMode={toggleMode} />
+        {/* <About/> */}
+        <About1 mode={mode} toggleMode={toggleMode} />
       </div>
     </>
   );
