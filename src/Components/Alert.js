b8038d800df3b72b420d699db3d28c5f42e47bc0
Ayush <ayushkansal303@gmail.com>
2023-06-15 23:29:21 +0530

    docs

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -8,7 +8,8 @@ export default function Alert(pass) {
     return (
         pass.alert && <div className={`alert alert-${pass.alert.type} alert-dismissible fade show`} role="alert">
             {capitalize(pass.alert.type)}, {pass.alert.msg}
-            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+            {/* the above cross button removed because closing delay added to alert */}
         </div>
     )
 }
